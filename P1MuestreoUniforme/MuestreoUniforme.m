b702d93655ef15314c8670501f802937c9cdfb5e
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-02-25 17:17:19 -0600

    Final Practica 1

--- P1MuestreoUniforme/MuestreoUniforme.m
+++ P1MuestreoUniforme/MuestreoUniforme.m
@@ -2,7 +2,10 @@ clear all
 close all
 
 t=0:.005:.5;
+w=-100:2:100-1;
 x = @(t) 0.5 + sin(4.*pi.*t) - cos(8.*pi.*t) + sin(12.*pi.*t);
+fs=20;
+d=0:1/fs:.5;
 
 %% Graficas de señal original en tiempo y frecuencia
 figure(1)
@@ -14,9 +17,8 @@ axis([0 .5 -1.6 2])
 grid on;
 
 fx=fftshift(fft(x(t),100));
-w=-100:2:100-1;
 figure(2)
-stem(w,abs(fx));
+stem(w,abs(fx),'fill','^');
 title('Señal en el dominio de la frecuencia')
 xlabel('$ f $','Interpreter','latex');
 ylabel('$ X(f) $','Interpreter','latex');
@@ -24,22 +26,19 @@ axis([-100 100 0 52])
 grid on;
 
 %% a) Muestreo ideal
-fs=20;
-d=0:1/fs:.5;
 h=pulstran(t,d,'rectpuls',0.001); % Función tren de pulsos unitarios
 muestreoIdeal=x(t).*h;
 
 figure(3)
-stem(t,muestreoIdeal);
+stem(t,muestreoIdeal,'Marker','none','LineWidth',1.5);
 title('Muestreo ideal en el dominio del tiempo')
 xlabel('$t$','Interpreter','latex');
 ylabel('$x_\delta(t)$','Interpreter','latex');
 grid on;
 
 fmi=fftshift(fft(muestreoIdeal,100));
-w=-100:2:100-1;
 figure(4);
-stem(w,abs(fmi));
+stem(w,abs(fmi),'fill','^');
 title('Muestreo ideal en el dominio de la frecuencia')
 xlabel('$ f $','Interpreter','latex');
 ylabel('$ X_\delta(f) $','Interpreter','latex');
@@ -57,13 +56,39 @@ ylabel('$x_n(t)$','Interpreter','latex');
 grid on;
 
 fn=fftshift(fft(muestreoNatural,100));
-w=-100:2:100-1;
 figure(6);
-stem(w,abs(fn));
+stem(w,abs(fn),'fill','^');
 title('Muestreo natural en el dominio de la frecuencia')
 xlabel('$ f $','Interpreter','latex');
 ylabel('$ X_n(f) $','Interpreter','latex');
 grid on;
 
 %% c) Muestreo de cresta plana
-  
+a=zeros(size(h));
+temp=0;
+for i=1:length(h)
+    if(temp==0 && h(i)==1 )
+        a(i)=x(t(i));
+        temp=x(t(i));
+    elseif(temp~=0 && h(i)==1 )
+        a(i)=temp;
+    elseif(temp~=0 && h(i)==0)
+        a(i)=0;
+        temp=0;
+    end  
+end
+
+figure(7)
+plot(t,a,'Linewidth',1.5);
+title('Muestreo de cresta plana en el dominio del tiempo')
+xlabel('$ t $','Interpreter','latex');
+ylabel('$ x_s(f) $','Interpreter','latex');
+grid on;
+
+f=fftshift(fft(a,100));
+figure(8)
+stem(w,abs(f),'fill','^');
+title('Muestreo de cresta plana en el dominio de la frecuencia')
+xlabel('$ f $','Interpreter','latex');
+ylabel('$ X_n(f) $','Interpreter','latex');
+grid on;
