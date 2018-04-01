27878c197ecf04bd52c7645c6e52912103e99c78
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-04-01 14:20:15 -0500

    Avance

---
+++ P4CuantizacionNoUniforme/P2_CD2.m
@@ -0,0 +1,57 @@
+clear all
+dt=0.005;
+t=0:dt:1;
+x=inline('sin(2*pi.*t)');
+xt=x(t);
+
+Ts=0.1;
+tren=[]
+for i=1:length(t)
+    
+        if mod(t(i),Ts)==0
+           tren(i)=1;
+        else
+            tren(i)=0;
+        end
+end
+
+
+mideal=xt.*tren;
+
+fs=20;
+T = 1/fs;
+ts = 0 : T : 1;
+xn = sin(2*pi*ts);
+
+
+%===================== Interpolador de orden cero =======================%
+
+%En tiempo 
+hcero=rectangularPulse((-1:dt:1)*fs);
+zoh=conv(mideal,hcero,'same');
+
+figure(6)
+interpolated1 = 0;
+for n = 1 : length(ts)
+    interpolated1 = interpolated1 + xn(n) * pulstran((t - (n - 1)*T)/T, 0.01, 'rectpuls');
+    hold on;
+    h1=plot(t, interpolated1, 'm--')
+end
+
+h2=plot(t,zoh,'g','LineWidth',2)
+hold on
+h3=plot(t,xt,'b','LineWidth',2)
+title('Señal recuperada por ZOH')
+xlabel('t')
+ylabel('x_r(t)')
+legend([h1 h2 h3],{'Interpolación','Recuperada','Original'})
+
+
+
+
+
+
+
+
+
+
--- P4CuantizacionNoUniforme/P4.m
+++ P4CuantizacionNoUniforme/P4.m
@@ -1,8 +1,9 @@
 close all;
 clear all;
-
+t1=0:.1:1;
 t=0:.001:2;
-x=sin(2*pi*t);
+x= sin(2*pi*t);
+
 
 %% Muestreo
 figure(1);
@@ -12,7 +13,7 @@ xlabel('$t$','Interpreter','Latex');
 ylabel('$x(t)$','Interpreter','Latex');
 grid on;
 
-Ts=0.05;
+Ts=0.1;
 trenPulsos=[];
 for i=1:length(t)
     if (mod(t(i),Ts)==0)
@@ -31,7 +32,7 @@ ylabel('$x_s(t)$','Interpreter','Latex');
 grid on;
 
 %% Cuantizacion
-nivelCuantizacion=2^13;
+nivelCuantizacion=8192;
 rangoDinamicoMax=1;
 rangoDinamicoMin=-1;
 [x,xc]=Nrud(nivelCuantizacion,rangoDinamicoMax,rangoDinamicoMin);
@@ -45,6 +46,71 @@ ylabel('$\hat{x}$','Interpreter','Latex');
 grid on;
 
 %% Codificacion
+Cod1=['0',num2str(dec2bin(4095*sin(2*pi*0.1)))];
+Cod2=['0',num2str(dec2bin(4095*sin(2*pi*0.2)))];
+Cod3=['1',num2str(dec2bin(4095*sin(2*pi*0.1)))];
+Cod4=['1',num2str(dec2bin(4095*sin(2*pi*0.2)))];
 
+num1=str2num(Cod1);
+format longG;
+num2=str2num(Cod2);
+format longG;
+num3=str2num(Cod3);
+format longG;
+num4=str2num(Cod4);
+format longG;
+
+%num3=num2str(zeros(1,12));
+
+Cod1
+Cod2
+Cod3
+Cod4
+
+Muestra={'Muestra 1';'Muestra 2';'Muestra 3';'Muestra 4';'Muestra 5';'Muestra 6';'Muestra 7';'Muestra 8';'Muestra 9';'Muestra 10'};
+Codigo=[num1;num2;num2;num1;0;num3;num4;num4;num3;0];
+Tab = table(Muestra,Codigo)
 
 %% Compresion Digital
+
+Comp1=['0111',Cod1(3),Cod1(4),Cod1(5),Cod1(6)]
+Comp2=['0111',Cod2(3),Cod2(4),Cod2(5),Cod2(6)]
+Comp3=['1111',Cod3(3),Cod3(4),Cod3(5),Cod3(6)]
+Comp4=['1111',Cod4(3),Cod4(4),Cod4(5),Cod4(6)]
+
+Co1=str2num(Comp1);
+format longG;
+Co2=str2num(Comp2);
+format longG;
+Co3=str2num(Comp3);
+format longG;
+Co4=str2num(Comp4);
+format longG;
+
+Muestra1 ={'Muestra 1';'Muestra 2';'Muestra 3';'Muestra 4';'Muestra 5';'Muestra 6';'Muestra 7';'Muestra 8';'Muestra 9';'Muestra 10'};
+Compresion =[Co1;Co2;Co2;Co1;0;Co3;Co4;Co4;Co3;0];
+Tab1 = table(Muestra1,Compresion)
+
+
+%%Expansion Digital
+Exp1=['01',Comp1(5),Comp1(6),Comp1(7),Comp1(8),'1000000']
+Exp2=['01',Comp2(5),Comp2(6),Comp2(7),Comp2(8),'1000000']
+Exp3=['11',Comp3(5),Comp3(6),Comp3(7),Comp3(8),'1000000']
+Exp4=['11',Comp4(5),Comp4(6),Comp4(7),Comp4(8),'1000000']
+
+Ex1=str2num(Exp1);
+format longG;
+Ex2=str2num(Exp2);
+format longG;
+Ex3=str2num(Exp3);
+format longG;
+Ex4=str2num(Exp4);
+format longG;
+
+Muestra2 ={'Muestra 1';'Muestra 2';'Muestra 3';'Muestra 4';'Muestra 5';'Muestra 6';'Muestra 7';'Muestra 8';'Muestra 9';'Muestra 10'};
+Expansion =[Ex1;Ex2;Ex2;Ex1;0;Ex3;Ex4;Ex4;Ex3;0];
+Tab1 = table(Muestra2,Expansion)
+
+
+%%Interpolador
+
