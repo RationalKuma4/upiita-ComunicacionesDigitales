a709951d923905a46be840860883b2e76a7dd899
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-05-22 22:08:03 -0500

    cambios

--- RxTx/Rx/Rx.ino
+++ RxTx/Rx/Rx.ino
@@ -1,143 +1,34 @@
-/* 
-  RF Blink - Receiver sketch 
-     Written by ScottC 17 Jun 2014
-     Arduino IDE version 1.0.5
-     Website: http://arduinobasics.blogspot.com
-     Receiver: XY-MK-5V
-     Description: A simple sketch used to test RF transmission/receiver.          
- ------------------------------------------------------------- */
-
- #define rfReceivePin A0  //RF Receiver pin = Analog pin 0
- #define ledPin 13        //Onboard LED = digital pin 13
-
- unsigned int data = 0;   // variable used to store received data
- const unsigned int upperThreshold = 70;  //upper threshold value
- const unsigned int lowerThreshold = 50;  //lower threshold value
-
- void setup(){
-   pinMode(ledPin, OUTPUT);
-   Serial.begin(9600);
- }
-
- void loop(){
-   data=analogRead(rfReceivePin);    //listen for data on Analog pin 0
-   
-    if(data>upperThreshold){
-     digitalWrite(ledPin, LOW);   //If a LOW signal is received, turn LED OFF
-     Serial.println(data);
-   }
-   
-   if(data<lowerThreshold){
-     digitalWrite(ledPin, HIGH);   //If a HIGH signal is received, turn LED ON
-     Serial.println(data);
-   }
- }
-
-/*#include <VirtualWire.h>
-
-void setup()
-{
-    Serial.begin(9600);  // Debugging only
-    Serial.println("Receptor: setup");
-
-    // Se inicializa el RF
-    vw_setup(2000);  // velocidad: Bits per segundo
-    vw_set_rx_pin(52);    //Pin 2 como entrada del RF
-    vw_rx_start();       // Se inicia como receptor
-}
-
-void loop()
-{
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
-    int dato1=0;
-    float dato2=0.0;
-    //verificamos si hay un dato valido en el RF
-    if (vw_get_message((uint8_t *)buf,&buflen))
-    {
-  int i;
-  String  DatoCadena="";
-        if((char)buf[0]=='i') //verificamos el inicio de trama
-        {
-            for (i = 1; i < buflen; i++)
-            {
-          DatoCadena.concat((char)buf[i]);
-            }
-            dato1=DatoCadena.toInt();
-            Serial.print("Dato1 recivido: ");
-            Serial.println(dato1);
-        }
-        else if((char)buf[0]=='f') //verificamos el inicio de trama
-        {
-            for (i = 1; i < buflen; i++)
-            {
-          DatoCadena.concat((char)buf[i]);
-            }
-            dato2=DatoCadena.toFloat();
-            Serial.print("Dato2 recivido: ");
-            Serial.println(dato2);
-        }
-    }
-}*/
-
-
-
-/*#include <VirtualWire.h>
-
-#define RX 0
-int rval=0;
+#include <VirtualWire.h>
+ 
+const int dataPin = 9;
+const int ledPin = 12;
  
 void setup()
 {
-    Serial.begin(9600);
-    Serial.println("Inicio de sketch - valores del potenciometro");
-    
     vw_setup(2000);
-    
-    vw_set_rx_pin(RX);
+    vw_set_rx_pin(dataPin);
     vw_rx_start();
+    
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, true);
+    delay(300);
+    digitalWrite(ledPin, false);
 }
  
 void loop()
 {
-  uint8_t buf[VW_MAX_MESSAGE_LEN];
-  uint8_t buflen = VW_MAX_MESSAGE_LEN;
-  int dato1=0, dato2=0, dato3=0;
-  //verificamos si hay un dato valido en el RF
-  if (vw_get_message((uint8_t *)buf,&buflen))
-  {
-    int i;
-    String  DatoCadena="";
-    if((char)buf[0]=='a') //potenciometro 1
+    uint8_t data;
+    uint8_t dataLength=1;
+ 
+    if (vw_get_message(&data,&dataLength))
     {
-        for (i = 1; i < buflen; i++)
+        if((char)data=='a')
         {
-          DatoCadena.concat((char)buf[i]);
+            digitalWrite(ledPin, true);
         }
-        dato1=DatoCadena.toInt();
-        Serial.print("Potenciometro 1: ");
-        Serial.println(dato1);
-    }
-    else if((char)buf[0]=='b') //verificamos el inicio de trama
-    {
-        for (i = 1; i < buflen; i++)
+        else if((char)data=='b')
         {
-          DatoCadena.concat((char)buf[i]);
-        }
-        dato2=DatoCadena.toFloat();
-        Serial.print("Potenciometro 2: ");
-        Serial.println(dato2);
+            digitalWrite(ledPin, false);
+        }            
     }
-    else if((char)buf[0]=='c') //verificamos el inicio de trama
-    {
-        for (i = 1; i < buflen; i++)
-        {
-          DatoCadena.concat((char)buf[i]);
-        }
-        dato2=DatoCadena.toFloat();
-        Serial.print("Potenciometro 3: ");
-        Serial.println(dato3);
-    }
-  }
 }
-*/
--- RxTx/Tx/Tx.ino
+++ RxTx/Tx/Tx.ino
@@ -1,159 +1,22 @@
-/* 
-  RF Blink - Transmit sketch 
-     Written by ScottC 17 Jun 2014
-     Arduino IDE version 1.0.5
-     Website: http://arduinobasics.blogspot.com
-     Transmitter: FS1000A/XY-FST
-     Description: A simple sketch used to test RF transmission.          
- ------------------------------------------------------------- */
-
- #define rfTransmitPin 4  //RF Transmitter pin = digital pin 4
- #define ledPin 13        //Onboard LED = digital pin 13
-
- void setup(){
-   pinMode(rfTransmitPin, OUTPUT);     
-   pinMode(ledPin, OUTPUT);    
- }
-
- void loop(){
-   for(int i=4000; i>5; i=i-(i/3)){
-     digitalWrite(rfTransmitPin, HIGH);     //Transmit a HIGH signal
-     digitalWrite(ledPin, HIGH);            //Turn the LED on
-     delay(2000);                           //Wait for 1 second
-     
-     digitalWrite(rfTransmitPin,LOW);      //Transmit a LOW signal
-     digitalWrite(ledPin, LOW);            //Turn the LED off
-     delay(i);                            //Variable delay
-   }
- }
-
-/*
-  433 MHz RF Module Transmitter Demonstration 1
-  RF-Xmit-Demo-1.ino
-  Demonstrates 433 MHz RF Transmitter Module
-  Use with Receiver Demonstration 1
+#include <VirtualWire.h>
  
-  DroneBot Workshop 2018
-  https://dronebotworkshop.com
-
- 
-// Include RadioHead Amplitude Shift Keying Library
-#include <RH_ASK.h>
-// Include dependant SPI Library 
-#include <SPI.h> 
- 
-// Create Amplitude Shift Keying Object
-RH_ASK rf_driver;
+const int dataPin = 9;
  
 void setup()
 {
-    // Initialize ASK Object
-    rf_driver.init();
+    Serial.begin(9600);     
+    vw_setup(2000);
+    vw_set_tx_pin(dataPin);
 }
  
 void loop()
 {
-    const char *msg = "Welcome to the Workshop!";
-    rf_driver.send((uint8_t *)msg, strlen(msg));
-    rf_driver.waitPacketSent();
-    delay(1000);
-}
-
-*/
-
-
-/*#include <VirtualWire.h>
-
-void setup()
-{
-    Serial.begin(9600);    // Debugging only
-    Serial.println("setup");
-
-    // Se inicializa el RF
-    vw_setup(2000); // velocidad: Bits per segundo
-    vw_set_tx_pin(12); //Pin 2 como salida para el RF 
-}
-
-void loop()
-{
-    char buf[VW_MAX_MESSAGE_LEN]; // Cadena para enviar
-    int dato1=millis()/1000; //variable con el tiempo en segundos
-    float dato2=3.14;   //segundo dato para enviar
-    String str="";  
-    
-    str="i"+String(dato1); //convertimos el entero a String y agramos un inicio de trama
-    str.toCharArray(buf,sizeof(buf)); //convertimos el String en un array
-    vw_send((uint8_t *)buf, strlen(buf)); //Enviamos el array
-    vw_wait_tx(); //Esperamos hasta que el mensaje se envie
-    Serial.println(buf);
-    
-    
-    str="f"+String(dato2); //convertimos el float a String y agramos un inicio de trama
-    str.toCharArray(buf,sizeof(buf)); //convertimos el String en un array
-    vw_send((uint8_t *)buf, strlen(buf)); ////Enviamos el array
-    vw_wait_tx(); //Esperamos hasta que el mensaje se envie
-
-
-    Serial.println(buf);
+    while (Serial.available() > 0) 
+    {
+      char data[1];
+      data[0] = Serial.read();
+      vw_send((uint8_t*)data,sizeof(data));
+      vw_wait_tx();         
+    }
     delay(200);
-}*/
-
-
-
-
-/*#include <VirtualWire.h>
-
-#define P1 A0
-#define P2 A1
-#define P3 A2
-#define Tx 12
-
-int pval1=0, pval2=0, pval3=0;
- 
-void setup() 
-{
-  Serial.begin(9600);
-  Serial.println("Inicio de sketch - valores del potenciometro");
-  pinMode(P1, INPUT);
-  pinMode(P2, INPUT);
-  pinMode(P3, INPUT);
-  pinMode(Tx, OUTPUT); 
-
-  vw_setup(2000);
-  vw_set_tx_pin(Tx);
 }
- 
-void loop() 
-{
-  delay(1000);
-  
-  pval1=analogRead(P1);
-  Serial.print("El valor es de 1 es = ");
-  Serial.println(pval1);
-
-  pval2=analogRead(P2);
-  Serial.print("El valor es de 2 es = ");
-  Serial.println(pval2);
-
-  pval3=analogRead(P3);
-  Serial.print("El valor es de 3 es = ");
-  Serial.println(pval3);
-  
-  char buf[VW_MAX_MESSAGE_LEN]; // Cadena para enviar
-  String str="";  
-  
-  str="a"+String(pval1); //convertimos el entero a String y agramos un inicio de trama
-  str.toCharArray(buf,sizeof(buf)); //convertimos el String en un array
-  vw_send((uint8_t *)buf, strlen(buf)); //Enviamos el array
-  vw_wait_tx(); //Esperamos hasta que el mensaje se envie
-  
-  str="b"+String(pval2); //convertimos el float a String y agramos un inicio de trama
-  str.toCharArray(buf,sizeof(buf)); //convertimos el String en un array
-  vw_send((uint8_t *)buf, strlen(buf)); ////Enviamos el array
-  vw_wait_tx(); //Esperamos hasta que el mensaje se envie
-
-  str="c"+String(pval3); //convertimos el float a String y agramos un inicio de trama
-  str.toCharArray(buf,sizeof(buf)); //convertimos el String en un array
-  vw_send((uint8_t *)buf, strlen(buf)); ////Enviamos el array
-  vw_wait_tx(); //Esperamos hasta que el mensaje se envie
-}*/
