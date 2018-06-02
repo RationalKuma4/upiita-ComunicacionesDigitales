9e8efb2cd90f0fb24930a6dfc7acee8fbaba103f
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-06-02 13:52:01 -0500

    h

--- RxTx/RxP/RxP.ino
+++ RxTx/RxP/RxP.ino
@@ -1,13 +1,12 @@
 #include <VirtualWire.h>
 #define dataPin 9
+int pot1,pot2,pot3;
  
 void setup()
 {
     Serial.begin(9600);
-    Serial.println("Inicio");
-    
-    vw_setup(2000);
     vw_set_rx_pin(dataPin);
+    vw_setup(2000);
     vw_rx_start();
 }
  
@@ -15,20 +14,17 @@ void loop()
 {
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
- 
-  // Recibir dato
+  String dataString;
   if (vw_get_message((uint8_t *)buf,&buflen))
   {
-    String dataString;
     if((char)buf[0]=='a')
     {
         for (int i = 1; i < buflen; i++)
         {
           dataString.concat((char)buf[i]);
         }
-        int dataInt = dataString.toInt();  // Convertir a int
-        Serial.print("Int: ");
-        Serial.println(dataInt);
+        pot1 = dataString.toInt(); 
+        Serial.println(String(pot1)+","+String(pot2)+","+String(pot3));
     }
     else if((char)buf[0]=='b')
     {
@@ -36,9 +32,8 @@ void loop()
         {
           dataString.concat((char)buf[i]);
         }
-        float dataFloat = dataString.toInt();  // Convertir a float
-        Serial.print("Float: ");
-        Serial.println(dataFloat);
+        pot2 = dataString.toInt();
+        Serial.println(String(pot1)+","+String(pot2)+","+String(pot3));
     }
     else if((char)buf[0]=='c')
     {
@@ -46,9 +41,8 @@ void loop()
         {
           dataString.concat((char)buf[i]);
         }
-        float dataFloat = dataString.toInt();  // Convertir a float
-        Serial.print("Float: ");
-        Serial.println(dataFloat);
+        pot3 = dataString.toInt();
+        Serial.println(String(pot1)+","+String(pot2)+","+String(pot3));
     }
   }
 }
