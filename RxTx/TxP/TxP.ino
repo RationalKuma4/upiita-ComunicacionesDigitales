4bcd435d37c18eb817a673db301cafad4c82a76d
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-06-02 13:51:32 -0500

    k

--- RxTx/TxP/TxP.ino
+++ RxTx/TxP/TxP.ino
@@ -9,45 +9,46 @@ int sensorValue1 = 0, sensorValue2 = 0, sensorValue3 = 0;
 void setup() 
 {
   Serial.begin(9600);
-  //Serial.println("Inicio");
-
-  vw_setup(2000);
+  
   vw_set_tx_pin(dataPin);
+  vw_setup(2000);
 }
 
 void loop() 
 {
   delay(1000);
   LeerPotenciometros();
-  TransmitirValores();
+  TransmitirValores(sensorValue1, sensorValue2, sensorValue3);
 }
 
 void LeerPotenciometros()
 {
-  int sensorValue1 = analogRead(P1);
-  int sensorValue2 = analogRead(P2);
-  int sensorValue3 = analogRead(P3);
-
+  sensorValue1 = analogRead(P1);
+  sensorValue2 = analogRead(P2);
+  sensorValue3 = analogRead(P3); 
   Serial.println(String(sensorValue1)+","+String(sensorValue2)+","+String(sensorValue3));
 }
 
-void TransmitirValores()
+void TransmitirValores(int sensorValue1, int sensorValue2, int sensorValue3)
 {
   String str;  
   char buf[VW_MAX_MESSAGE_LEN];
-   
+
+  delay(10);
   // Envio P1
   str = "a" + String(sensorValue1); /// Convertir a string
   str.toCharArray(buf,sizeof(buf)); // Convertir a char array
   vw_send((uint8_t *)buf, strlen(buf)); // Enviar array
   vw_wait_tx(); // Esperar envio
 
+  
   // Envio P2
   str = "b" + String(sensorValue2); /// Convertir a string
   str.toCharArray(buf,sizeof(buf)); // Convertir a char array
   vw_send((uint8_t *)buf, strlen(buf)); // Enviar array
   vw_wait_tx(); // Esperar envio
 
+  
   // Envio P3
   str = "c" + String(sensorValue3); /// Convertir a string
   str.toCharArray(buf,sizeof(buf)); // Convertir a char array
