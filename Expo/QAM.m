6b944e5455e4b53df2a7ab14580c5da9d76e8d76
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-06-07 14:16:04 -0500

    Expo

--- Expo/QAM.m
+++ Expo/QAM.m
@@ -24,13 +24,3 @@ xlabel('Fase');ylabel('Cuadratura');
 grid on;
 
 
-M = 256;                         % Modulation order
-x = (0:M-1)';                    % Integer input
-y1 = qammod(x,M,'bin');        % 16-QAM output
-scatterplot(y1);
-text(real(y1)+0.1, imag(y1), dec2bin(x));
-title('256-QAM');
-axis([-16 16 -16 16]);
-xlabel('Fase');ylabel('Cuadratura');
-grid on;
-
---
+++ Expo/Qam2.m
@@ -0,0 +1,36 @@
+clear all;
+close all;
+clc;
+
+M = 16;                     % Size of signal constellation
+k = log2(M);                % Number of bits per symbol
+n = 30000;                  % Number of bits to process
+numSamplesPerSymbol = 1;    % Oversampling factor
+
+rng default                 % Use default random number generator
+dataIn = randi([0 1],n,1);  % Generate vector of binary data
+
+stem(dataIn(1:40),'filled');
+title('Random Bits');
+xlabel('Bit Index');
+ylabel('Binary Value');
+
+dataInMatrix = reshape(dataIn,length(dataIn)/k,k);   % Reshape data into binary k-tuples, k = log2(M)
+dataSymbolsIn = bi2de(dataInMatrix);                 % Convert to intege
+
+dataMod = qammod(dataSymbolsIn,M,'bin');         % Binary coding, phase offset = 0
+dataModG = qammod(dataSymbolsIn,M); % Gray coding, phase offset = 0
+
+EbNo = 10;
+snr = EbNo + 10*log10(k) - 10*log10(numSamplesPerSymbol);
+
+receivedSignal = awgn(dataMod,snr,'measured');
+receivedSignalG = awgn(dataModG,snr,'measured');
+
+sPlotFig = scatterplot(receivedSignal,1,0,'g.');
+hold on
+grid on;
+scatterplot(dataMod,1,0,'k*',sPlotFig)
+text(real(dataMod)+0.1, imag(dataMod), dec2bin(n))
+
+
